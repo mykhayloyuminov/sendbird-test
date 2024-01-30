e69255002b593d0a28695d370ec99e93f0b2ee92
mykhayloyuminov <mykhaylo.yuminov@gmail.com>
2024-01-30 01:11:43 +0100

    sendbird test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '@sendbird/uikit-react/dist/index.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <Chat
+              userId={config.userId}
+              nickname={config.nickname}
+              theme={config.theme}
+              useCustomQuery={config.useCustomQuery}
+            />
+          </Route>
+          <Route path="/">
+            <Login onSubmit={setconfig} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
